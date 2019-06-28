440301e0724daadbf45aa659a90dd2ad002f9a84
Iarton <airton.brasil@fatec.sp.gov.br>
2019-06-28 01:13:10 -0300

    Add info tab

--- app/src/main/java/brasil/airton/kotlinapp/MainActivity.kt
+++ app/src/main/java/brasil/airton/kotlinapp/MainActivity.kt
@@ -2,11 +2,19 @@ package brasil.airton.kotlinapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnInfo.setOnClickListener(){
+            startActivity (Intent(this, InfoActivity :: class.java))
+        }
+
     }
 }
